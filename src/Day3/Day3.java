ed69c1fa6bf914591371fae6b59b273c458b58a1
yasin <ysnclk2020@gmail.com>
2023-11-03 16:17:57 +0100

    second commit

---
+++ src/Day3/Day3.java
@@ -0,0 +1,7 @@
+package Day3;
+
+public class Day3 {
+    public static void main(String[] args) {
+        System.out.println("day3");
+    }
+}
--- src/day2/calisma2.java
+++ src/day2/calisma2.java
@@ -3,5 +3,6 @@ package day2;
 public class calisma2 {
     public static void main(String[] args) {
         System.out.println("calisma2");
+        System.out.println("x");
     }
 }
